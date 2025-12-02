db64bfe4e12169a8de850136ad13cc4fef3b9fe5
Aman Ahamed <amanahamed04@gmail.com>
2025-12-02 13:05:02 +0530

    category CRUD functionality

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 from . import views
 from blogs import views as blogsView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
@@ -30,4 +31,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+
+    # Dashboadrd URLs
+    path('dashboard/', include('dashboards.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
